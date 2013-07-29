1e3a9cae80462ee3a65deaa88f015b0d9dfe550b
ylx199011 <ylx199011@gmail.com>
2013-07-29 20:12:40 +0800

    Update save_popup.rb
    before using SavePopup#set,you have to assign an absolute path to ATT::Config.root(i.e.  ATT::Config.root = file_save_path #file_save_path is an absolute path)

--- lib/popup/save_popup.rb
+++ lib/popup/save_popup.rb
@@ -2,10 +2,14 @@ module ATT
   # for save as dialog
   class SavePopup < FilePopup
 
-    # Save diaglog as a file 
-    # if +file_path+ is relative path, it will be expand to abosulte path join with ATT::Config.root
+    # Save dialog as a file
+    # if +file_path+ is nil,it will take the default name given by dialog.elsif +file_path+ is relative path, it will be expand to abosulte path join with ATT::Config.root
     # @param: file_path
-    def set(file_path)
+    def set(file_path='')
+      if file_path.to_s.strip.length == 0 # It's nil, empty, or just whitespace
+        file_name = text_field.value
+        file_path = File.join(Config.root, file_name)
+      end
       check_file_not_exist?(file_path)
       set_text(file_path)
       click_confirm
